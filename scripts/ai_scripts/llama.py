75baa8d2343c66e8d20c985fdee11053cb40d05c
Ruurd Bijlsma <ruurd@bijlsma.dev>
2024-11-06 23:27:02 +0100

    Update todo, add stable diffusion test scripts.

--- scripts/ai_scripts/llama.py
+++ scripts/ai_scripts/llama.py
@@ -1,10 +1,10 @@
 from llama_cpp import Llama
 
 llm = Llama(
-    model_path="./scripts/model/Llama-3.2-3B-Instruct-Q6_K.gguf",
-    n_gpu_layers=-1, # Uncomment to use GPU acceleration
+    model_path="../model/Llama-3.2-3B-Instruct-Q6_K.gguf",
+    n_gpu_layers=-1,  # Uncomment to use GPU acceleration
     # seed=1337, # Uncomment to set a specific seed
-    n_ctx=2048, # Uncomment to increase the context window
+    n_ctx=2048,  # Uncomment to increase the context window
 )
 chat_prefix = "Keep your answers short and direct, ideally one sentence."
 conversation = [chat_prefix]
--- scripts/ai_scripts/question_image.py
+++ scripts/ai_scripts/question_image.py
@@ -16,31 +16,24 @@ model = model.to(device='cuda', dtype=torch.float16)
 tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-V-2', trust_remote_code=True)
 model.eval()
 
-img_url = Path('ocr.jpg')
+img_url = Path('../../data/images/1/20170726_152057.jpg')
 image = Image.open(img_url).convert("RGB")
 
 questions = [
-    "You are an OCR bot, extract the text from this image.",  # werkt niet goed
-    "what search terms would you use to find this image?",
-    "Perfectly describe this photo in detail, leave nothing out. The original image will be recreated from your response.",
+    "You are a scene recognition model trained on places 365. What is the scene in this image? Answer using at most 2 words.",
+    "what search terms would you use to find this image? Answer with the terms only, separated by commas, give at least 3 terms.",
+    "Perfectly describe this photo in detail, leave nothing out. The original image will be recreated from your response. Don't embellish your response just keep it factual.",
+
     "You are a photographer who's creating a selection of images to show to family. "
     "The selection should have high quality and interesting images that tell part of the story. "
-    "Should this photo be included in the album? Indicate with a number from 1 (no) to 10 (absolutely).",
-    "Rate this photo on a scale of 1 to 10 in terms of professional quality standards? Give the number only.",
-    "Does this photo appear to be taken indoors or outdoors? If outdoors, can you recognize the location or "
-    "type of place (e.g., park, beach, city)? Keep your answer short and confident.",
-    "Does this image appear to be taken during the day or night? Can you estimate the time of day (morning, afternoon, evening)? Keep your answer short and confident.",
-    "What kind of weather is visible in this photo? Does it indicate a specific season (e.g., winter, summer)? Keep your answer short and confident.",
-    "What activity is happening in this photo? (e.g., walking, playing, working) Keep your answer short and confident.",
-    "How many people are in this photo? Just give a number.",
-    "What emotions are visible on the faces in this photo (e.g., happiness, surprise, sadness)? Keep your answer short and confident.",
-    "Describe the clothing or fashion style of the person in this photo.",
-    "What are the main objects visible in this photo? (e.g., dog, car, table)",
-    "What are the dominant colors in this photo?",
-    "How memorable is this image, on a scale from 1 to 10? Just give a number.",
-    "If there is a person in this photo, how old are they? Give an exact integer number. If there isn't a person, respond with -1.",
-    "Does this photo capture strong emotions or sentimental moments?",
-    "What event or occasion does this image seem to represent? (e.g., birthday, vacation, celebration) Keep your answer short and confident.",
+    "Should this photo be included in the album? Indicate with a number from 1 (no) to 10 (absolutely). "
+    "Only answer with the number",
+
+    "Rate this photo on a scale of 1 to 10 in terms of professional quality standards? Only answer with the number.",
+
+    "You are a activity recognition model. What is the activity displayed in this image? Answer using at most 2 words.",
+
+    "You are a occasion and event recognition model. What is the occasion or event displayed in this image? e.g., birthday, vacation, graduation. Answer using at most 2 words.",
 ]
 for question in questions:
     msgs = [
