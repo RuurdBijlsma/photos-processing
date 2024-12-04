e4e9a4b598f1062d84fdbacad37f662242253af7
Ruurd Bijlsma <ruurd@bijlsma.dev>
2024-12-04 21:56:47 +0100

    Add pyscan dependency checking to checks.ps1

--- scripts/checks.ps1
+++ scripts/checks.ps1
@@ -1,12 +1,13 @@
 Push-Location $PSScriptRoot/..
 
 uv run ruff check . --fix
+uv run pyscan
 uv run mypy -p app --strict --ignore-missing-imports
 uv run mypy -p tests --strict --ignore-missing-imports
 uv run pylint `
     --disable=C0114,C0115,C0116,R0903,C0415,E1101,E1102,R0917,R0913,C0103,R0901,C0411 `
     --output-format=colorized `
-    --fail-under 10 `
+    --fail-under 9 `
     app/**/*.py tests/**/*.py
 
 Pop-Location
