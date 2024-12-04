f687968a14d9541d465170d7fcdae2ac28bf8f4b
Ruurd Bijlsma <ruurd@bijlsma.dev>
2024-12-04 21:45:29 +0100

    Improve pylint workflow.

--- scripts/checks.ps1
+++ scripts/checks.ps1
@@ -5,8 +5,8 @@ uv run mypy -p app --strict --ignore-missing-imports
 uv run mypy -p tests --strict --ignore-missing-imports
 uv run pylint `
     --disable=C0114,C0115,C0116,R0903,C0415,E1101,E1102,R0917,R0913,C0103,R0901,C0411 `
-    --exit-zero `
     --output-format=colorized `
+    --fail-under 10 `
     app/**/*.py tests/**/*.py
 
 Pop-Location
