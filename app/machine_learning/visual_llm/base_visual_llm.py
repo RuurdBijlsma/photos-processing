c864c0a7ca4f6e89a87fa94923903f95b9664e44
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-01-15 13:04:15 +0100

    Fix some pylint errors.

--- app/machine_learning/visual_llm/base_visual_llm.py
+++ app/machine_learning/visual_llm/base_visual_llm.py
@@ -20,15 +20,11 @@ class ChatMessage:
 
 
 class BaseVisualLLM(ABC):
-
     def image_question(self, image: Image, question: str) -> str:
         return self.images_question([image], question)
 
     def images_question(self, images: list[Image], question: str) -> str:
-        answer = ""
-        for msg in self.stream_chat([ChatMessage(message=question, images=images)]):
-            answer += msg
-        return answer
+        return str.join("", self.stream_chat([ChatMessage(message=question, images=images)]))
 
     @abstractmethod
     def stream_chat(
--- scripts/checks.ps1
+++ scripts/checks.ps1
@@ -5,7 +5,6 @@ uv run pyscan
 uv run mypy -p app --strict --ignore-missing-imports
 uv run mypy -p tests --strict --ignore-missing-imports
 uv run pylint `
-    --disable=C0114,C0115,C0116,R0903,C0415,E1101,E1102,R0917,R0913,C0103,R0901,C0411 `
     --output-format=colorized `
     --fail-under 9 `
     app/**/*.py tests/**/*.py
